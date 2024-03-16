fb5bc43c7e5acbb5c5e1603ffda286490e13aef3
engineerapurba96 <apurbaranjan.tech@gmail.com>
2024-03-16 21:35:54 +0530

    api completed

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-const mongoose = require('mongoose');
+const express = require("express");
+require("./config");
+const Product = require("./product");
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017/e-comm');
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String
-
+app.use(express.json());
+app.post("/create", async (req, resp) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(req.body);
+  resp.send(result);
 });
 
-const saveInDB = async () => {
-    const Product = mongoose.model('products', productSchema);
-    let data = new Product({
-        name: "max 100",
-        price: 200,
-        brand: 'maxx',
-        category: 'Mobile'
-    });
-    const result = await data.save();
-    console.log(result);
-}
+app.get("/list", async (req, resp) => {
+    let data = await Product.find();
+    resp.send(data);
+})
 
-const updateInDB =async  () => {
-    const Product = mongoose.model('products', productSchema);
-    let data =await  Product.updateOne(
-        { name: "max 100" },
-        {
-            $set: { price: 590,name:'max pro 8' }
-        }
-    )
-    console.log(data)
-}
+app.delete("/delete/:_id", async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.deleteOne(req.params);
+    resp.send(data);
+})
 
+app.put("/update/:_id",async (req, resp) => {
+    console.log(req.params)
+    let data = await Product.updateOne(
+        req.params,
+        {$set: req.body}
+    );
+    resp.send(data);
+})
 
-const deleteInDB = async ()=>{
-    const Product = mongoose.model('products', productSchema);
-    let data = await Product.deleteOne({name:'max pro 6'})
-    console.log(data);
-}
-const findInDB = async ()=>{
-    const Product = mongoose.model('products', productSchema);
-    let data = await Product.find({name:'max pro 611'})
-    console.log(data);
-}
-
-findInDB();
+app.listen(5000);
