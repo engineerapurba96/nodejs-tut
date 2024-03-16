8938b084d6a881f3043f11642e2c5cd12ee349d6
engineerapurba96 <apurbaranjan.tech@gmail.com>
2024-03-16 16:42:21 +0530

    mongoose started

--- delete.js
+++
@@ -1,8 +0,0 @@
-const dbConnect = require('./mongodb');
-
-const deleteData=async ()=>{
-let data = await dbConnect();
-let result = await data.deleteMany({name:'max'})
-console.log(result)
-}
-deleteData();
